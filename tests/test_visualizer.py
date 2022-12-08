baaf059ae7f96764717befa0ef2c3380c8b5cba2
Alexander Nasuta <alexander.nasuta@rwth-aachen.de>
2022-12-08 13:06:12 +0100

    fixed test colors

--- tests/test_visualizer.py
+++ tests/test_visualizer.py
@@ -7,7 +7,7 @@ def test_terminal_gantt_vis(visualisation_dataframe):
 
     c_map = plt.cm.get_cmap("jet")  # select the desired cmap
     arr = np.linspace(0, 1, 4)  # create a list with numbers from 0 to 1 with n items
-    colors = {f"Machine {resource}": c_map(val)[1:] for resource, val in enumerate(arr)}
+    colors = {f"Machine {resource}": c_map(val)[:3] for resource, val in enumerate(arr)}
 
     visualizer = DisjunctiveGraphJspVisualizer()
 
@@ -23,7 +23,7 @@ def test_window_gantt_vis(visualisation_dataframe):
 
     c_map = plt.cm.get_cmap("jet")  # select the desired cmap
     arr = np.linspace(0, 1, 4)  # create a list with numbers from 0 to 1 with n items
-    colors = {f"Machine {resource}": c_map(val)[1:] for resource, val in enumerate(arr)}
+    colors = {f"Machine {resource}": c_map(val)[:3] for resource, val in enumerate(arr)}
 
     visualizer = DisjunctiveGraphJspVisualizer()
 
